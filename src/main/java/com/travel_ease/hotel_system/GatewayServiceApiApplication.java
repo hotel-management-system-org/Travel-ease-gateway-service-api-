c622558b54f0bd58942a25362482db67d25f04bf
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-09 20:57:31 +0530

    feat: add GatewayServiceApiApplication as Spring Boot entry point

---
+++ src/main/java/com/travel_ease/hotel_system/GatewayServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.travel_ease.hotel_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApiApplication.class, args);
+	}
+
+}
